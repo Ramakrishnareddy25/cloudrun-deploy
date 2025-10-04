6530e97953534460519ccf6118d26a75b74596a5
Ramakrishnareddy25 <ramakrishnareddytetali@gmail.com>
2025-10-04 16:09:31 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,18 @@
+resource "google_storage_bucket" "static-site" {
+  name          = "ram-bucket-test"
+  location      = "europe-west-2"
+  force_destroy = true
 
+  uniform_bucket_level_access = true
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+  cors {
+    origin          = ["http://image-store.com"]
+    method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
+    response_header = ["*"]
+    max_age_seconds = 3600
+  }
+}
