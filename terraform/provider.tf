3333a8887a488b61150fe031e605c18e8877acae
Ramakrishnareddy25 <ramakrishnareddytetali@gmail.com>
2025-10-04 15:43:46 +0530

    Create provider.tf

---
+++ terraform/provider.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = "1.9.2"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.28.0"
+    }
+  }
+}
+
+
+provider "google" {
+  region      = "us-central1"
+  credentials = "./key.json"
+  project     = "xenon-alliance-465110-v6"
+}
+
+terraform {
+  backend "gcs" {
+    bucket  = "run-sources-xenon-alliance-465110-v6-europe-west2"
+    prefix  = "terraform/state"
+  }
+}
